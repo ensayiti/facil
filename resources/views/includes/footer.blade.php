b1bffbc330364a136e919155e457ea2ca6581eea
XEM <itstaureans@gmail.com>
2023-08-14 16:14:13 +0700

    feat (Home): Added Hero & CTA

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer footer-center p-2 bg-base-100 text-base-content">
+    <div>
+        <p>© 2023 fácil by <a href="https://twitter.com/ensayiti" class="underline">ensayiti</a>.</p>
+    </div>
+</footer>
