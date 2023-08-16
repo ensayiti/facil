330114b39563e904b4178c4412d1aee4b017382b
XEM <itstaureans@gmail.com>
2023-08-16 14:43:53 +0700

    Fix (Home): Change navbar style to responsive

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,7 +1,7 @@
-    <div class="navbar bg-base-100">
+    <div class="navbar bg-foreground border-b">
         <div class="navbar-start">
             <div class="dropdown">
-                <label tabindex="0" class="btn btn-ghost lg:hidden">
+                <label tabindex="0" class="btn btn-ghost text-primary-foreground lg:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -15,16 +15,16 @@
                     <li><a>Contact</a></li>
                 </ul>
             </div>
-            <a class="btn btn-ghost rounded-xl hover:btn-primary normal-case text-xl">fácil</a>
+            <a class="btn btn-ghost btn-sm rounded-xl text-primary-foreground hover:btn-primary normal-case text-xl">fácil</a>
         </div>
         <div class="navbar-center hidden lg:flex">
-            <ul class="menu menu-horizontal px-1">
-                <li><a>Home</a></li>
-                <li><a>About</a></li>
-                <li><a>Contact</a></li>
+            <ul class="menu menu-horizontal space-x-2">
+                <li><a class="font-semibold text-primary-foreground hover:bg-primary-foreground">Home</a></li>
+                <li><a class="font-semibold text-primary-foreground hover:bg-primary-foreground">About</a></li>
+                <li><a class="font-semibold text-primary-foreground hover:bg-primary-foreground">Contact</a></li>
             </ul>
         </div>
         <div class="navbar-end">
-            <a href="/admin" class="btn rounded-xl hover:btn-primary">Dashboard</a>
+            <a href="/admin" class="btn btn-sm rounded-lg hover:btn-primary">Dashboard</a>
         </div>
     </div>
