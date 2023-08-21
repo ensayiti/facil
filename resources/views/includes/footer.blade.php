816c435900ad426068ad74d2320630dc91c8b69c
XEM <itstaureans@gmail.com>
2023-08-21 17:49:11 +0700

    Rebuild

--- resources/views/includes/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer footer-center p-2 bg-foreground text-primary-foreground border-t">
-    <div>
-        <p>© 2023 fácil by <a href="https://twitter.com/ensayiti" class="underline">ensayiti</a>.</p>
-    </div>
-</footer>
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.home');
-});
-
-Route::get('/contact', function () {
-    return view('pages.contact');
+    return view('welcome');
 });
